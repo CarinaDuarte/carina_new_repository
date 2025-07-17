54f52bcbf10304d5b8bd59f0e4ffd8f8dad5c236
CarinaDuarte <154423907+CarinaDuarte@users.noreply.github.com>
2025-07-17 17:36:26 +0100

    Update Program.cs 1

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace SQLi_1
             {
                 var user = args[0];
                 var pwd = Encrypt(args[1]);
-                //Login(user, pwd);
+                Login(user, pwd);
 		//Login1(user, pwd);
 		     
             }
@@ -34,27 +34,27 @@ namespace SQLi_1
             return plain;
         } 
 
-//         private static void Login(string username,string password)
-//        {
-//             try
-//            {
-//                using (var conn = new SqlConnection("conn..."))
-//                {
-//                   var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
-//                    using (var cmd = new SqlCommand(sql))
-//                    {
-//                        cmd.Connection = conn;
-//                        cmd.ExecuteScalar();
-//                    }
-//
-//                }
-//            }
-//            catch  
-//            {
-//
-//                Console.WriteLine("An error has occurred !!");
-//            } 
-//        } 
+         private static void Login(string username,string password)
+        {
+             try
+            {
+                using (var conn = new SqlConnection("conn..."))
+                {
+                   var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
+                    using (var cmd = new SqlCommand(sql))
+                    {
+                        cmd.Connection = conn;
+                        cmd.ExecuteScalar();
+                    }
+
+                }
+            }
+            catch  
+            {
+
+                Console.WriteLine("An error has occurred !!");
+            } 
+        } 
 
 //         private static void Login1(string username,string password)
 //        {
